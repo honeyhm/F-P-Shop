31862992312f7be7841c761df3fea80b17bb8932
maleki <honeyhm@gmail.com>
2020-07-05 15:03:51 +0430

    first attemp to be a functional programmer

---
+++ main.js
@@ -0,0 +1,88 @@
+// Amazon Shopping
+
+/*IMPLEMENT A CART FEATURE:
+1. Add items to cart
+2. Add 30% tax to item in cart
+3. Buy item: cart ---> purchases
+4. Empty cart 
+*/
+
+const user={
+    name: 'honey',
+    active: true,
+    cart: [],
+    purchases: []
+}
+
+//st like DB for logs
+let amazonHistory = []
+
+// // //first scenario:
+// //this one includes all 4 other tasks (like compose)
+// console.log(purchaseItem(user, {name: 'laptop', price:344}))
+// function purchaseItem(user, item){
+//    return Object.assign({},user,{purchases: item})
+// } 
+
+// // //second scenario:
+// //using compose(right to left)
+// //because we have more than 2 funcs to pass as arguments,
+// //it is better to use libraryslike lamda?!
+const compose= (f,g) => (...args) =>f(g(...args));
+const pipe = (f, g) => (...args) => g(f(...args));
+// // it is a factory!
+console.log(purchaseItem(
+    emptyCart,
+    buyItem,
+    applyTaxToItems,
+    addItemToCart
+)(user, { name: 'laptop', price: 200 }))
+
+function purchaseItem(...fns){
+   return fns.reduce(compose)// or pipe
+}
+
+
+
+function addItemToCart(user,item){
+    amazonHistory.push(user)
+    const updateCart = user.cart.concat(item)
+    return Object.assign({}, user, { cart: updateCart })
+}
+
+function applyTaxToItems(user){
+    amazonHistory.push(user)
+    //using {cart} instead of user.cart
+    const {cart}= user;
+    const taxRate = 1.3;
+    const updatedCart = cart.map(item =>{
+        return{
+            name: item.name, 
+            price: item.price*taxRate
+        }
+    })
+    return Object.assign({}, user, {cart: updatedCart})
+}
+
+function buyItem(user){
+    amazonHistory.push(user)
+    return Object.assign({}, user, {purchases: user.cart})
+}
+
+function emptyCart(user){
+    amazonHistory.push(user)
+    return Object.assign({}, user, {cart: []} )
+}
+
+console.log(amazonHistory)
+function refundItem(){
+
+}  
+
+function getUserState()
+
+function goForward()
+
+function changeStatus(){
+
+}
